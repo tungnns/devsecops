a4b60f40554c6f28c0e2e7c1951d3204d451f1db
tungnns <tungnns@gmail.com>
2023-11-04 17:35:43 +0700

    fix trivy scan script syntax

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -6,8 +6,8 @@ echo $dockerImageName
 # docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 # docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-trivy image --exit-code 0 --severity HIGH --light $dockerImageName
-trivy image --exit-code 1 --severity CRITICAL --light $dockerImageName
+image --exit-code 0 --severity HIGH --light $dockerImageName
+image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
     # Trivy scan result processing
     exit_code=$?
